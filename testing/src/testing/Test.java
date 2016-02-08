9c923487dcc09b13ef8bc7aa20b088e0e8f4ffdf
Modi <cosjp@Mobile-PC>
2016-02-08 18:26:03 -0500

    Initial input

---
+++ testing/src/testing/Test.java
@@ -0,0 +1,29 @@
+package testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+/*		String st = "12";
+		
+		int i = Integer.parseInt(st);
+		//System.out.println(Integer.parseInt(st));
+*/
+		double x = 1.9;
+		double res = (x-2)/((x*x)-4);
+		
+		System.out.println(res);
+		
+	}
+/*	public static double harmonic (int i){
+		if (i < 1){
+			return -1.0;}
+		
+		if (i == 1){
+			return 1.0;}
+		
+		return (1.0 / i) + harmonic(i-1);
+		
+		
+	}*/
+
+}
